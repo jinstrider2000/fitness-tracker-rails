7beca5f1b0eaa7021359a52d0da401c8050a7117
jinstrider2000 <efrainperezjr@live.com>
2017-09-27 18:08:26 -0400

    added more helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,10 +17,14 @@ module ApplicationHelper
 
   def print_time(datetime)
     ordinal_suffix = datetime.localtime.day.ordinal
-    datetime.localtime.strftime("%B %-d<sup>#{ordinal_suffix}</sup>, %Y")
+    datetime.localtime.strftime("%B %-d<sup>#{ordinal_suffix}</sup>, %Y").html_safe
   end
 
   def print_time_index_style(datetime)
     datetime.localtime.strftime("%-m/ %d/ %y")
+  end
+
+  def link_status(link)
+    request.path_info =~ /#{link}/ ? "active" : ""
   end
 end
