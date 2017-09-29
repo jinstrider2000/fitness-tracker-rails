9a391283da486d56ef7835026ba7ac5768bfdb9c
jinstrider2000 <efrainperezjr@live.com>
2017-09-29 19:15:25 -0400

    deleted unecessary files and added footer to layout

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,5 +1,9 @@
 class Achievement < ApplicationRecord
-  belongs_to :activity, polymorphic: true
+  belongs_to :activity, polymorphic: true, dependent: :destroy
   belongs_to :user
   validates_presence_of :activity
+
+  def activity_attributes=(values)
+    
+  end
 end
