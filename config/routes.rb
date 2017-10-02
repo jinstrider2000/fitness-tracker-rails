18cfa1947d37f289ee183c3ad4bb5ca85e2bfa35
jinstrider2000 <efrainperezjr@live.com>
2017-10-02 17:45:07 -0400

    set up some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  resources :foods
-  resources :exercises
-  resources :achievements
   devise_for :users
+  resources :users, only: [:index]
+  resources :achievements, only: [:new, :index]
+  resources :foods, only: [:create, :edit, :update, :destroy]
+  resources :exercises, only: [:create, :edit, :update, :destroy]
+  resources :users, param: :slug do
+    resources :achievements, only: [:index]
+    resources :foods, only: [:index]
+    resources :exercises, only: [:index]
+    get '/followers' => 'users#index'
+    get '/following' => 'users#index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
