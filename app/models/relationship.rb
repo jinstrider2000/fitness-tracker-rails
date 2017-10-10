27bda5e3ea9cf1b7cd786a473daf547399d66d68
jinstrider2000 <efrainperezjr@live.com>
2017-10-10 19:16:02 -0400

    added block and unblock functions

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,14 +4,24 @@ class Relationship < ApplicationRecord
 
   before_save :update_following_each_other
 
+  def block
+    blocked = true
+  end
+
+  def unblock
+    blocked = false
+  end
+
   private
 
   def update_following_each_other
     reciprocal_relationship = Relationship.find_by(follower: self.followee, followee: self.follower)
-    if !self.blocked? && reciprocal_relationship.present?
+    if !self.blocked? && reciprocal_relationship.present? && !reciprocal_relationship.blocked?
       self.following_each_other = true
+      reciprocal_relationship.update_column(:following_each_other,true)
     else
       self.following_each_other = false
+      reciprocal_relationship.update_column(:following_each_other,false)
     end
   end
 end
