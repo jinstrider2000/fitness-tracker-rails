2ecd4fa3ac74ba8af8d3cdc50e43b26c417bd3a4
jinstrider2000 <efrainperezjr@live.com>
2017-10-19 00:48:23 -0400

    updated routes yet again :(

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index]
-  resources :achievements, only: [:new, :create ,:index, :destroy]
-  resources :foods, only: [:edit, :update, :destroy]
-  resources :exercises, only: [:edit, :update, :destroy]
-  resources :users, param: :slug do
-    get '/' => 'users#show'
-    get '/blocked-users' = 'users#index'
-    get '/muted-users' = 'users#index'
+  resources :achievements, only: [:new, :create, :destroy]
+  resources :foods, only: [:show, :edit, :update, :destroy]
+  resources :exercises, only: [:show, :edit, :update, :destroy]
+  resources :users, param: :slug, only: [:show] do
+    member do
+      get 'blocked', as: "blocked_users"
+      get 'muted', as: 'muted_users'
+      get 'feed', as: 'user_feed'
+      get 'followers', 'followers'
+      get 'following', 'following'
+    end
     resources :achievements, only: [:index]
     resources :foods, only: [:index]
     resources :exercises, only: [:index]
-    get '/followers' => 'users#index'
-    get '/following' => 'users#index'
     post '/follow' => 'relationships#create'
     delete '/unfollow' => 'relationships#destroy'
     post '/block' => 'blocks#create'
@@ -20,5 +22,6 @@ Rails.application.routes.draw do
     post '/mute' => 'mutes#create'
     delete '/unmute' => 'mutes#destroy'
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
