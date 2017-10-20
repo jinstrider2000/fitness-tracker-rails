6dc226cce744f01fad50b586c70ab6c5247f740b
jinstrider2000 <efrainperezjr@live.com>
2017-10-20 18:10:16 -0400

    tweak routes for umpteenth time, removed user members block for custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index]
-  resources :achievements, only: [:new, :create, :destroy]
+  resources :achievements, only: [:new, :create]
   resources :foods, only: [:show, :edit, :update, :destroy]
   resources :exercises, only: [:show, :edit, :update, :destroy]
   resources :users, param: :slug, only: [:show] do
-    member do
-      get 'blocked', as: "blocked_users"
-      get 'muted', as: 'muted_users'
-      get 'feed', as: 'user_feed'
-      get 'followers', as: 'followers'
-      get 'following', as: 'following'
-    end
     resources :achievements, only: [:index]
     resources :foods, only: [:index]
     resources :exercises, only: [:index]
@@ -21,6 +14,11 @@ Rails.application.routes.draw do
     delete '/unblock' => 'blocks#destroy'
     post '/mute' => 'mutes#create'
     delete '/unmute' => 'mutes#destroy'
+    get '/blocked' => 'users#blocked'
+    get '/muted' => 'users#muted'
+    get '/activity-feed' => 'users#activity_feed'
+    get '/followers' => 'users#followers'
+    get '/following' => 'users#following'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
