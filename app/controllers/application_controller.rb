ef78af947f7c26301c7e08b99aba1b6b023ec040
jinstrider2000 <efrainperezjr@live.com>
2017-10-25 01:15:16 -0400

    added devise recommended methods for permitting sanitized params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+  before_action :configure_sign_up_parameters, if: :devise_controller?, only: [:create]
+  before_action :configure_account_update_parameters, if: :devise_controller?, only: [:update]
+  
+  protected
+
+  def configure_sign_up_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :daily_calorie_intake_goal])
+  end
+
+  def configure_account_update_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :daily_calorie_intake_goal])
+  end
 
 end
