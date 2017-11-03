c8755b0b427c0863d3c136308750395b25bc14bf
jinstrider2000 <efrainperezjr@live.com>
2017-11-03 08:14:35 -0400

    just corrected a line

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -46,8 +46,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :daily_calorie_goal
-    ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :daily_calorie_goal])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
