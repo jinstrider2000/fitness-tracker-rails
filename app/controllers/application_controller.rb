78fa1aa110b3b9f077a458883119d4ac9235f588
jinstrider2000 <efrainperezjr@live.com>
2018-01-25 08:29:05 -0500

    scoped routes for locale variable according to rails guides and devise docs, also created before action to set locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_locale
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   
   protected
@@ -10,6 +10,15 @@ class ApplicationController < ActionController::Base
     policy_name = exception.policy.class.to_s.underscore
     flash[:error] = t "#{policy_name}.#{exception.query}", scope: :pundit, default: :default_msg
     redirect_to(request.referrer || root_path)
+  end
+  # Got these methods from Rails Guides
+  # http://guides.rubyonrails.org/i18n.html#managing-the-locale-across-requests
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,42 @@
 Rails.application.routes.draw do
+
   root 'news_feed#index'
-  get '/news-feed' => 'news_feed#index'
-  devise_for :users, controllers: {registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks'}, skip: [:passwords]
+  get '/:locale' => 'news_feed#index'
+
+  scope ':locale', constraints: {locale: /en|es/} do
+    get '/news-feed' => 'news_feed#index'
+    devise_for :users, controllers: {registrations: 'users/registrations'}, skip: [:passwords, :omniauth_callbacks]
+  end
+
+  devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}, scope: '/(:locale)', locale: /en|es/ do
+    devise_for :users, skip: :omniauth_callbacks
+  end  
   
-  resources :users, param: :slug, only: [:index, :show], module: "users", controller: "user_actions" do
-    member do
-      post 'follow'
-      delete 'unfollow'
-      post 'block'
-      delete 'unblock'
-      post 'mute'
-      delete 'unmute'
-      get 'blocked'
-      get 'muted'
-      get 'followers'
-      get 'following'
+  scope ':locale', constraints: {locale: /en|es/} do
+    resources :users, param: :slug, only: [:index, :show], module: "users", controller: "user_actions" do
+      member do
+        post 'follow'
+        delete 'unfollow'
+        post 'block'
+        delete 'unblock'
+        post 'mute'
+        delete 'unmute'
+        get 'blocked'
+        get 'muted'
+        get 'followers'
+        get 'following'
+      end
     end
   end
 
-  scope 'users/:slug', as: 'user' do
-    resources :achievements, only: [:new, :index]
+  scope ':locale', constraints: {locale: /en|es/} do
+    scope 'users/:slug', as: 'user' do
+      resources :achievements, only: [:new, :index]
+    end
   end
-  
-  resources :achievements, except: [:new, :index]
 
+  scope ':locale', constraints: {locale: /en|es/} do
+    resources :achievements, except: [:new, :index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
