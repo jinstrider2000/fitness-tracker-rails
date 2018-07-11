25d50219ed5d5dd5b61d60f810ec2a906d94e81d
jinstrider2000 <efrainperezjr@live.com>
2018-07-10 20:59:38 -0400

    starting user.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,6 +2,12 @@ function setCurrentUser() {
   $.get("/current-user", user => window.currentUser = user );
 }
 
+function getUserIndex() {
+  $.get(`${window.location.pathname}.json`, function (users) {
+    
+  });
+}
+
 function userIndexInit() {
   
 }
