d1df5646552b805377b98ae230704ee1a87345a1
jinstrider2000 <efrainperezjr@live.com>
2018-07-26 13:17:13 -0400

    setting up newsFeed update interval method

--- app/assets/javascripts/newsFeed.js
+++ app/assets/javascripts/newsFeed.js
@@ -1,7 +1,34 @@
 class NewsFeedItem {
   constructor(init) {
-    this.name = init;
+    Object.assign(this, init);
   }
+
+  get timeSinceCreated() {
+    return "Hey!"
+  }
+}
+
+function newsFeedUpdateInit() {
+  if (!window.lastFeedItemCreated) {
+    window.lastFeedItemCreated = $("#news-feed-items-container > div:first-child").data("created-at");
+  }
+  if (!window.newFeedUpdateInterval) {
+    window.newFeedUpdateInterval = setInterval(function () {
+      window.newsFeedLastUpdate = new Date(Date.now());
+      console.log(window.newsFeedLastUpdate.toISOString());
+    }, 5000)
+  }
+  handlebarsNewsFeedIndexInit();
+}
+
+function handlebarsNewsFeedIndexInit() {
+  if (!window.newsFeedItemTemplate) {
+    window.newsFeedItemTemplate = Handlebars.compile($("#news-feed-item-template").html());
+  }
+}
+
+function tester(params) {
+  
 }
 
 function seconds_since_epoch() {
