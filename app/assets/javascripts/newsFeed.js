7af0e5363236691cc35ba7f797b228dcd87c465f
jinstrider2000 <efrainperezjr@live.com>
2018-07-28 23:31:51 -0400

    fixed newsfeed updating issues, used turbolinks:load event

--- app/assets/javascripts/newsFeed.js
+++ app/assets/javascripts/newsFeed.js
@@ -5,21 +5,21 @@ class NewsFeedItem {
   }
 
   static sinceCreated(timeCreated) {
-    const timeDifferenceSeconds = (window.lastTimeFeedUpdated - timeCreated)/1000;
+    const timeDifferenceSeconds = parseInt((window.lastTimeFeedUpdated - timeCreated)/1000);
     if (timeDifferenceSeconds >= 0 && timeDifferenceSeconds <= 5) {
-      return I18n.t("news_feed.news_feed_helper.time_since_created.now")
+      return I18n.t("news_feed.news_feed_helper.time_since_created.now");
     } else if(timeDifferenceSeconds >= 6 && timeDifferenceSeconds <= 59) {
-      return I18n.t("news_feed.news_feed_helper.time_since_created.sec", {count: timeDifferenceSeconds})
+      return I18n.t("news_feed.news_feed_helper.time_since_created.sec", {count: timeDifferenceSeconds});
     } else if(timeDifferenceSeconds >= 60 && timeDifferenceSeconds <= 3599) {
-      return I18n.t("news_feed.news_feed_helper.time_since_created.min", {count: timeDifferenceSeconds/60})
+      return I18n.t("news_feed.news_feed_helper.time_since_created.min", {count: parseInt(timeDifferenceSeconds/60)});
     } else if(timeDifferenceSeconds >= 3600 && timeDifferenceSeconds <= 86399) {
-      return I18n.t("news_feed.news_feed_helper.time_since_created.hour", {count: timeDifferenceSeconds/3600})
+      return I18n.t("news_feed.news_feed_helper.time_since_created.hour", {count: parseInt(timeDifferenceSeconds/3600)});
     } else if(timeDifferenceSeconds >= 86400 && timeDifferenceSeconds <= 2627999) {
-      return I18n.t("news_feed.news_feed_helper.time_since_created.day", {count: timeDifferenceSeconds/86400})
+      return I18n.t("news_feed.news_feed_helper.time_since_created.day", {count: parseInt(timeDifferenceSeconds/86400)});
     } else if(timeDifferenceSeconds >= 2628000 && timeDifferenceSeconds <= 31535999) {
-      return I18n.t("news_feed.news_feed_helper.time_since_created.month", {count: timeDifferenceSeconds/2628000})
+      return I18n.t("news_feed.news_feed_helper.time_since_created.month", {count: parseInt(timeDifferenceSeconds/2628000)});
     } else {
-      return I18n.t("news_feed.news_feed_helper.time_since_created.year", {count: timeDifferenceSeconds/31536000})
+      return I18n.t("news_feed.news_feed_helper.time_since_created.year", {count: parseInt(timeDifferenceSeconds/31536000)});
     }
   }
 
@@ -48,19 +48,28 @@ function newsFeedUpdateInit() {
   if (!window.newFeedUpdateInterval) {
     window.newFeedUpdateInterval = setInterval(updateNewsFeed, 60000);
   }
+  $("a").on("click", resetFeedVars);
+  $("#lang-selection-form").on("submit", resetFeedVars);
 }
 
 function updateNewsFeed() {
-  if (window.location.pathname === "/" || window.location.pathname === `/${I18n.locale}/news-feed`) {
     window.lastTimeFeedUpdated = new Date(Date.now());
+    for (const timeSpan of $(".time-since-created")) {
+      timeSpan.innerHTML = NewsFeedItem.sinceCreated(window.feedItemDates[timeSpan.parentElement.id]);
+    }
     $.get(`/${I18n.locale}/news-feed.json?latest_id=${window.lastFeedItemID}`, function (newsFeedItems, status, responseObj) {
-      console.log(this);
+      if (newsFeedItems.length > 0) {
+        
+      } else {
+        
+      }
     }).fail(ajaxErrorMessage);
-  } else {
-    clearInterval(window.newFeedUpdateInterval);
-    window.newFeedUpdateInterval = null;
-    window.numOfFeedItemsPending = null;
-    window.lastFeedItemID = null;
-    window.feedItemDates = null;
-  }
+}
+
+function resetFeedVars() {
+  clearInterval(window.newFeedUpdateInterval);
+  window.newFeedUpdateInterval = null;
+  window.numOfFeedItemsPending = null;
+  window.lastFeedItemID = null;
+  window.feedItemDates = null;
 }
