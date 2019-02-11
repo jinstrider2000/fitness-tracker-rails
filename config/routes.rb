1a3de7509bac3c8412336eba03e00f5d68443f4b
jinstrider2000 <efrainperezjr@live.com>
2019-02-11 16:11:00 -0500

    took locale scope out of app

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@ Rails.application.routes.draw do
 
   root 'news_feed#index'
   get '/current-user' => Users::UserActionsController.action(:current_user_json), as: "current_user_json"
-  get '/:locale' => 'news_feed#index'
+  # get '/:locale' => 'news_feed#index'
 
-  scope ':locale', constraints: {locale: /en|es/} do
+  # scope ':locale', constraints: {locale: /en|es/} do
     get '/news-feed' => 'news_feed#index'
     devise_for :users, controllers: {registrations: 'users/registrations'}, skip: [:passwords, :omniauth_callbacks]
-  end
+  # end
 
   devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}, scope: '/(:locale)', locale: /en|es/ do
     devise_for :users, skip: :omniauth_callbacks
   end  
   
-  scope ':locale', constraints: {locale: /en|es/} do
+  # scope ':locale', constraints: {locale: /en|es/} do
     resources :users, param: :slug, only: [:index, :show], module: "users", controller: "user_actions" do
       member do
         post 'follow'
@@ -31,9 +31,9 @@ Rails.application.routes.draw do
         get 'most-active-today' => 'user_actions#most_active_today'
       end
     end
-  end
+  # end
 
-  scope ':locale', constraints: {locale: /en|es/} do
+  # scope ':locale', constraints: {locale: /en|es/} do
     scope 'users/:slug', as: 'user' do
       resources :achievements, param: :id, only: [:new, :index] do
         member do
@@ -45,10 +45,10 @@ Rails.application.routes.draw do
         get 'new-form-fields' => 'achievements#new_form_fields'
       end
     end
-  end
+  # end
 
-  scope ':locale', constraints: {locale: /en|es/} do
+  # scope ':locale', constraints: {locale: /en|es/} do
     resources :achievements, except: [:new, :index]
-  end
+  # end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
